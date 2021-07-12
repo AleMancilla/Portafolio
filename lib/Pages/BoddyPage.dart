efb4acc9909a15c36e626a250d04f5ec6d3a49a6
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2021-07-12 01:22:47 -0400

    page navigation complete

--- lib/Pages/BoddyPage.dart
+++ lib/Pages/BoddyPage.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:portafolio/Core/Provider/UtilsProvider.dart';
+import 'package:portafolio/Pages/BodyPages/About.dart';
+import 'package:portafolio/Pages/BodyPages/Contact.dart';
+import 'package:portafolio/Pages/BodyPages/Projects.dart';
+import 'package:portafolio/Pages/BodyPages/Skills.dart';
+import 'package:portafolio/Utils/UtilsDesign.dart';
+import 'package:provider/provider.dart';
 
-class BoddyPage extends StatelessWidget {
+class BoddyPage extends StatefulWidget {
+  @override
+  _BoddyPageState createState() => _BoddyPageState();
+}
+
+class _BoddyPageState extends State<BoddyPage> {
+  UtilsProvider utilsProvider;
+  @override
+  void initState() {
+    utilsProvider = Provider.of<UtilsProvider>(context, listen: false);
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    utilsProvider = Provider.of<UtilsProvider>(context, listen: true);
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-    );
+    return _body();
+  }
+
+  Widget _body() {
+    switch (utilsProvider.indexPage) {
+      case IndexPage.About:
+        return About();
+        break;
+      case IndexPage.Contact:
+        return Contact();
+        break;
+      case IndexPage.Projects:
+        return Projects();
+        break;
+      case IndexPage.Skills:
+        return Skills();
+        break;
+      default:
+        return About();
+    }
   }
 }
